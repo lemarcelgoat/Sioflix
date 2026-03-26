8a9bb0d6560a2903714e1f94e73fb408752f8d03
KCShinorik <emerik.guichard@gmail.com>
2026-03-26 14:52:37 +0100

    correctif

--- php/search.php
+++ php/search.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Recherche</title>
-
+    <link rel="stylesheet" href="../css/search.css">
 </head>
 <body>
     <a href="../index.php">
